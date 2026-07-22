3dbe83e6a18adf86cbc7eb4914ba377d3ba1b4fb
izhaan02 <152889654+izhaan02@users.noreply.github.com>
2026-07-22 14:23:45 +0530

    Time: 2 ms (50.67%), Space: 9.9 MB (26.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,19 +3,33 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
 
-        int freq[26]={0};
+        // int freq[26]={0};
 
+        // for(int i=0;i<s.size();i++){
+        //     freq[s[i]-'a']++;
+        // }
+
+        // for(int i=0;i<t.size();i++){
+        //     if(freq[t[i]-'a']>0){
+        //         freq[t[i]-'a']--;
+        //     }
+        //     else return false;
+        // }
+        // return true;
+
+        unordered_map<char,int>mp;
         for(int i=0;i<s.size();i++){
-            freq[s[i]-'a']++;
+            mp[s[i]]++;
         }
 
         for(int i=0;i<t.size();i++){
-            if(freq[t[i]-'a']>0){
-                freq[t[i]-'a']--;
+            if(mp[t[i]]>0){
+                mp[t[i]]--;
             }
             else return false;
         }
         return true;
+
 
     }
 };
