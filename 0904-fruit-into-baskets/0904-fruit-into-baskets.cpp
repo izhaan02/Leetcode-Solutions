a96ff07d4d6f238550417ff35fc6ba0b03d91b86
izhaan02 <152889654+izhaan02@users.noreply.github.com>
2026-07-29 15:48:40 +0530

    Time: 50 ms (30.35%), Space: 88.9 MB (7.23%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        // int a =fruits[i];
-        // int b;
-        // int i=0;
-        // int j=0;
-        // int basket1=0;
-        // int basket2=0;
-
-        // while(i<fruits.size()){
-        //     a=fruits[i];
-            
-
-        //     while(j<friuts.size()){
-        //         if(fruits[i]==a) {
-        //             i++;
-        //             j++;
-        //         }
-        //     }
-        // }
-        int maxi=0;
         int left=0;
+        int ans=0;
         map<int,int>mp;
-        
-        for(int i=0;i<fruits.size();i++){
-            mp[fruits[i]]++;
+
+        for(int right=0;right<fruits.size();right++){
+            mp[fruits[right]]++;
 
             while(mp.size()>2){
                 mp[fruits[left]]--;
                 if(mp[fruits[left]]==0) mp.erase(fruits[left]);
                 left++;
-
             }
-            maxi=max(maxi,i-left+1);
+            ans=max(ans,right-left+1);
+
         }
-        return maxi;
+        return ans;
     }
 };
