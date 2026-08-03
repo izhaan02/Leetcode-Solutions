56a92cc8737c6449c897fb420052edbb3d2034de
izhaan02 <152889654+izhaan02@users.noreply.github.com>
2026-08-03 14:57:57 +0530

    Time: 19 ms (14.35%), Space: 19.6 MB (28.59%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    int solve(vector<int>& piles, int i, int j){
-        if(i==j) return piles[i];
+    int solve(vector<int>&piles,int i, int j){
+        if(i>=j) return piles[i];
 
-        int takeleft=piles[i]-solve(piles,i+1,j);
-        int takeright=piles[j]-solve(piles,i,j-1);
+        int choice1=piles[i]-solve(piles,i+1,j);
+        int choice2=piles[j]-solve(piles,i,j-1);
+        return max(choice1,choice2);
 
-        return max(takeleft,takeright);
     }
-    int solveUsingMem(vector<int>& piles, int i, int j, vector<vector<int>>&dp){
-        if(i==j) return piles[i];
+    int solveUsingMem(vector<int>&piles,int i, int j, vector<vector<int>>&dp){
+        if(i>=j) return piles[i];
 
         if(dp[i][j]!=INT_MIN) return dp[i][j];
 
-        int takeleft=piles[i]-solveUsingMem(piles,i+1,j,dp);
-        int takeright=piles[j]-solveUsingMem(piles,i,j-1,dp);
+        int choice1=piles[i]-solveUsingMem(piles,i+1,j,dp);
+        int choice2=piles[j]-solveUsingMem(piles,i,j-1,dp);
+        return dp[i][j]=max(choice1,choice2);
 
-        return dp[i][j]=max(takeleft,takeright);
     }
-
     bool stoneGame(vector<int>& piles) {
-        vector<vector<int>>dp(piles.size(),vector<int>(piles.size(),INT_MIN));
-        int ans=solveUsingMem(piles,0,piles.size()-1,dp);
-        return ans>0;
 
+        int i=0;
+        int j=piles.size()-1;
+        vector<vector<int>>dp(piles.size(),vector<int>(piles.size(),INT_MIN));
+        int ans=solveUsingMem(piles,i,j,dp);
+
+        
+        return ans>0;
     }
 };
