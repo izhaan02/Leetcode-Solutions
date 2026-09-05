859367369b6d6626c1413ea472cae0610a4b71d0
izhaan02 <152889654+izhaan02@users.noreply.github.com>
2026-09-05 12:00:02 +0530

    Time: 75 ms (44.21%), Space: 111.5 MB (28.2%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,9 +4,7 @@ public:
         unordered_map<int,int>mp;
 
         for(int i=0;i<nums.size();i++){
-            if(mp.find(nums[i])!=mp.end()){
-                return true;
-            }
+            if(mp.find(nums[i])!=mp.end()) return true;
             mp[nums[i]]++;
         }
         return false;
