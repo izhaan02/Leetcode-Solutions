a7233a9f983201b672591f95306e8bba1254f77c
izhaan02 <152889654+izhaan02@users.noreply.github.com>
2026-09-12 16:21:17 +0530

    Time: 11 ms (60.92%), Space: 14.1 MB (50.73%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -5,8 +5,7 @@ public:
 
         int n=nums.size();
         int minDiff=INT_MAX;
-        int finalSum=0;
-        
+        int ans=-1;
         for(int i=0;i<n-2;i++){
 
             if(i>0 && nums[i]==nums[i-1]) continue;
@@ -14,15 +13,13 @@ public:
             int j=i+1;
             
             int k=n-1;
-            
+
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
-                int currDiff=abs(target-sum);
-
-
-                if(currDiff==0){
-                    return sum;
-                    // ans.push_back({nums[i],nums[j],nums[k]});
+                int diff=abs(target-sum);
+                if(diff<minDiff){
+                    minDiff=diff;
+                    ans=sum;
                     // while(j<k && nums[j]==nums[j+1]) j++;
                     // while(k>j && nums[k]==nums[k-1]) k--;
                     // j++;
@@ -30,16 +27,12 @@ public:
 
 
                 }
-                else if (target-sum>0) j++;
-                else if(target-sum<0) k--;
-                if(currDiff<minDiff){
-                    minDiff=currDiff;
-                    finalSum=sum;
-                }
+                if (sum>target) k--;
+                else j++;
             }
 
             
         }
-        return finalSum;
+        return ans;
     }
 };
