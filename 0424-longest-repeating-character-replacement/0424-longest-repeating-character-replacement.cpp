c476dd7e25122a0ccedaffa4d121fae4eada868c
izhaan02 <152889654+izhaan02@users.noreply.github.com>
2026-09-24 22:36:22 +0530

    Time: 3 ms (66.84%), Space: 10.7 MB (93.09%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // int ans=0;
-        // for(int i=0;i<s.length();i++){
-        //     int maxi=0;
-        //     int freq[26]={0};
-        //     for(int j=i;j<s.length();j++){
-        //         freq[s[j]-'A']++;
-        //         maxi=max(maxi,freq[s[j]-'A']);
-        //         int change=j-i+1-maxi;
-        //         if(change<=k) ans=max(ans,j-i+1);
-        //         else break;
-        //     }
-        // }
-        // return ans;
-
-        int ans=0;
-        int maxi=0;
+      
+        int n=s.length();
         int left=0;
+        int ans=0;
         int freq[26]={0};
-        for(int right=0;right<s.length();right++){
-            freq[s[right]-'A']++;
-            maxi=max(maxi, freq[s[right]-'A']);
+        int maxFreq=0;
 
-            while(right-left+1-maxi>k){
+
+        for(int right=0;right<n;right++){
+
+            freq[s[right]-'A']++;
+            maxFreq=max(maxFreq,freq[s[right]-'A']);
+
+            while((right-left+1)-maxFreq>k){
                 freq[s[left]-'A']--;
-                maxi=0;
-                for(int i=0;i<26;i++) maxi=max(maxi,freq[i]);
                 left++;
+
             }
             ans=max(ans,right-left+1);
+
         }
         return ans;
     }
